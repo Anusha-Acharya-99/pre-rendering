6baafaf4510925aedc528416460b5c9ea29822a7
Anusha Acharya <anusha.acharya@robosoftin.com>
2022-11-07 10:28:10 +0530

    static generation

---
+++ pages/users.js
@@ -0,0 +1,29 @@
+import User from './components/user';
+
+const UserList = ({ users }) => {
+  return (
+    <>
+      <h1>Users</h1>
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <User user={user} />
+          </div>
+        );
+      })}
+    </>
+  );
+};
+
+export default UserList;
+
+export async function getStaticProps() {
+  const reponse = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await reponse.json();
+  console.log(data);
+  return {
+    props: {
+      users: data,
+    },
+  };
+}
